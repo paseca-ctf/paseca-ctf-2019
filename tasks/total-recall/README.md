0e46a31cdb18d480d00f41e98550ca4be57554d1
Andrey Masyanov <masyanovm22@gmail.com>
2019-08-17 10:32:25 +0300

    Modified encryption script for total-recall
    Now support multiencryption (4 lulz ofc)

---
+++ tasks/total-recall/README.md
@@ -0,0 +1,13 @@
+# Total Recall
+### Ready dump link
+(Download)[]
+## Tear up
+* Get Windows 10 vm
+* Download Python 3 and [Belkasoft Live RAM Caputer](https://belkasoft.com/ram-capturer), install them, reboot
+* Run local web server to host script for encryption (maybe add this webserver to hosts as some meatspin or smth..)
+* Download the script inside vm, download flag
+* Install pycrypto inside virtualenv in vm
+* Encrypt several times
+* Dump it
+
+> I da, eto ne ya anglichanin, a shrifty russkie v arche sleteli
--- tasks/total-recall/src/enc.py
+++ tasks/total-recall/src/enc.py
@@ -20,22 +20,28 @@ class AESCipher(object):
         enc = base64.b64decode(enc)
         iv = enc[:AES.block_size]
         cipher = AES.new(self.key, AES.MODE_CBC, iv)
-        return self._unpad(cipher.decrypt(enc[AES.block_size:])).decode('utf-8')
+        return self._unpad(cipher.decrypt(enc[AES.block_size:])).decode('wtf-8')
 
     def _pad(self, s):
-        return s + (self.bs - len(s) % self.bs) * chr(self.bs - len(s) % self.bs)
+        return s + (self.bs - len(s) % self.bs) * bytes([self.bs - len(s) % self.bs])
 
     @staticmethod
     def _unpad(s):
         return s[:-ord(s[len(s)-1:])]
 
-def main():
-	source = sys.stdin.buffer
-	b = source.read()
-	n = '\xd50\x9e\xe6\xf6\n\xa4\xbf'
-	a = AESCipher(n)
-	return a.encrypt(b)
-
-
+def encrypt(data):
+    n = 'totalpasecatotalcrypto324'
+    a = AESCipher(n)
+    return a.encrypt(data)
+	
 if __name__ == "__main__":
-	main()
+    img = sys.argv[1]
+    while True:
+        keyprss = input("Press enter to encrypt it..")
+        sys.stdout.flush()
+        with open(img, "rb") as f:
+            d = encrypt(f.read())
+        with open(img, "wb") as f:
+            f.write(d)
+        print("Encryp777ed!!\x18\x21")
+
