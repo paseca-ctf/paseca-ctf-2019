dbe88cf1f86b331f80f154134de372d584ba70e9
Alexey Kharlamov <der@2-47.ru>
2019-08-24 16:56:57 +0200

    subway deployed

--- tasks/subway/server/subway/__init__.py
+++ tasks/subway/server/subway/__init__.py
@@ -2,11 +2,13 @@ import secrets
 import posixpath
 from flask import Flask
 import os
+import matplotlib
+matplotlib.use('Agg')
 
 app = Flask(__name__)
 if not os.path.exists(".secret"):
-    open(".secret", "wb").write(secrets.token_bytes(16))
-app.config["SECRET_KEY"] = open(".secret", "rb").read()
+    open("/var/subway/.secret", "wb").write(secrets.token_bytes(16))
+app.config["SECRET_KEY"] = open("/var/subway/.secret", "rb").read()
 app.config["N_TASKS"] = 5
 app.config["SVG_DIR"] = posixpath.abspath(posixpath.join(posixpath.curdir, "tasks"))
 
