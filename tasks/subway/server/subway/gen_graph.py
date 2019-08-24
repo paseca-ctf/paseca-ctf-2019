f59a6ce9b7b85082c8a10c072706326f97246b53
Maxim <supermax74.02@gmail.com>
2019-08-24 12:20:29 +0500

    Subway still needs testing

--- tasks/subway/server/subway/gen_graph.py
+++ tasks/subway/server/subway/gen_graph.py
@@ -9,8 +9,8 @@ import pandas as pd
 import os
 import json
 
-import subway.dsu
 from subway import app
+import subway.dsu
 
 warnings.filterwarnings("ignore")
 
@@ -25,7 +25,6 @@ def gen_graph(n_stations: list):
     lines_names = list([i.title() for i in names])
     node_names = [lines_names[sum(n_stations[:c]):sum(n_stations[:c]) + stations] for c, stations in
                   zip(itertools.count(), n_stations)]
-    lines = []
     for c, stations in zip(itertools.count(), n_stations):
         G.add_nodes_from(node_names[c], color=c / (len(n_stations) - 1))
         for f, t in zip(node_names[c][:-1], node_names[c][1:]):
@@ -44,24 +43,26 @@ def gen_graph(n_stations: list):
 
 
 def gen_svg_from_graph(g, pos, path):
-    cmap = plt.cm.get_cmap('gist_rainbow')
+    cmap = plt.cm.get_cmap('rainbow')
     node_colors = [cmap(i[1]['color']) if i[1]['color'] != -1 else (0.2, 0.2, 0.2, 1.0) for i in g.nodes(data=True)]
     edge_colors = [cmap(i[2]['color']) if i[2]['color'] != -1 else (0.2, 0.2, 0.2, 1.0) for i in g.edges(data=True)]
     edge_labels = {(i[0], i[1]): i[2]['length'] for i in g.edges(data=True)}
-    s = len(g.nodes()) // 5
-    plt.figure(figsize=(s, s))
-    nx.draw_networkx_edge_labels(g, pos, edge_labels=edge_labels)
-    nx.draw(g, pos,
+    fig, ax = plt.subplots(figsize=(35, 35), frameon=False)
+    nx.draw_networkx_edge_labels(g, pos, edge_labels=edge_labels, ax=ax)
+    nx.draw_networkx(g, pos,
             with_labels=True,
             node_size=300,
             node_color=node_colors,
             edge_color=edge_colors,
-            width=10.0)
-    plt.savefig(path, format='svg')
+            width=10.0,
+            ax=ax)
+    ax.axis([-1.05, 1.05, -1.05, 1.05])
+    ax.grid(True, which='both')
+    fig.savefig(path, format='svg', bbox_inches = 'tight', pad_inches = 0)
 
 
 def find_ans(G, start):
-    return nx.single_source_dijkstra(G, start)
+    return nx.single_source_dijkstra(G, start, weight="length")
 
 
 def gen_task(g):
--- tasks/subway/server/subway/views.py
+++ tasks/subway/server/subway/views.py
@@ -1,9 +1,11 @@
-from flask import render_template, abort, session, request, send_from_directory, redirect, url_for, safe_join, send_file
+from flask import render_template, session, request, redirect, url_for, safe_join, send_file, flash
 
 from subway import app
 from subway.gen_graph import gen_task
 import json
 
+tasks = [[2]*3, [3]*7, [5]*7, [11]*13, [15]*17]
+tasks = [[15]*17]
 
 @app.route('/tasks/<id>')
 def return_task(id):
@@ -11,16 +13,42 @@ def return_task(id):
     print(filename)
     return send_file(filename)
 
-
 @app.route('/', methods=["GET", "POST"])
 def index():
     if "solved" not in session:
         session["solved"] = 0
+    if session["solved"] >= len(tasks):
+        return "Thanks. Here is your payout: PASECA{bZZzZ_d1d_u_u53_dijkstra}"
     if not session.get("cur_task", None):
-        id = gen_task([10] * 17)
+        id = gen_task(tasks[session["solved"]])
         session["cur_task"] = id
     with open(safe_join(app.config["SVG_DIR"], session["cur_task"] + '.json')) as f:
         t = json.load(f)
     if request.method == "POST":
+        if "another" in request.form:
+            session["cur_task"] = None
+        else:
+            ans = dict(request.form)
+            del ans['submit']
+            correct = True
+            for station, dist in ans.items():
+                try:
+                    dist = int(dist)
+                except ValueError:
+                    flash(f"Distance on station {station} is not an integer")
+                    correct = False
+                    continue
+                if dist != t['ans'][station]:
+                    flash(f"Station {station} is not {dist} minutes away from {t['start']}")
+                    correct = False
+            if len(t['ans']) != len(ans):
+                correct = False
+                flash(f"Hey! Don't try to hack this task."
+                f"The correct sollution does not envolve anything except using the browser")
+            if correct:
+                session["solved"] += 1
+                session["cur_task"] = None
+                flash("Good job!")
+            print(ans)
         return redirect(url_for("index"))
     return render_template("index.html", pos=t['pos'], start=t['start'])
